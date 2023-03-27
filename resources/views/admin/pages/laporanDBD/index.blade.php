bc1a97cb8e6b5aec93748284bee0f378cc3c9fd1
wahyunf354 <wahyunurfadillah313@gmail.con>
2023-03-28 06:26:38 +0700

    selesai membuat upload laporan dan menampilkannnya

--- resources/views/admin/pages/laporanDBD/index.blade.php
+++ resources/views/admin/pages/laporanDBD/index.blade.php
@@ -80,15 +80,6 @@
         "responsive": true, "lengthChange": false, "autoWidth": false,
         "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
       }).buttons().container().appendTo('#table_laporan_dbd_file_wrapper .col-md-6:eq(0)');
-      // $('#example2').DataTable({
-      //   "paging": true,
-      //   "lengthChange": false,
-      //   "searching": false,
-      //   "ordering": true,
-      //   "info": true,
-      //   "autoWidth": false,
-      //   "responsive": true,
-      // });
     });
   </script>
 @endsection
